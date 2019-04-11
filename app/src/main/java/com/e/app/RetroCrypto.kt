6354c6ed3ec4d5cc03a082b867bba90f7dbdcce3
Navneet Prajapati <navneet.prajapati@continental-corporation.com>
2019-04-11 18:23:12 +0800

    implementing retrofit

---
+++ app/src/main/java/com/e/app/RetroCrypto.kt
@@ -0,0 +1,3 @@
+package com.e.app
+
+data class RetroCrypto(val currency : String, val price : String)
--- app/src/main/java/com/e/app/api/GetData.kt
+++ app/src/main/java/com/e/app/api/GetData.kt
@@ -1,4 +1,10 @@
 package com.e.app.api
 
+import com.e.app.RetroCrypto
+import io.reactivex.Observable
+import retrofit2.http.GET
+
 interface GetData {
+    @GET("/api/users")
+    fun getContacts() : Observable<List<RetroCrypto>>
 }
